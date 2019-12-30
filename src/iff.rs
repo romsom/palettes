e056b5c23484985011409184dfe80fbc63a67790
Roman Sommer <roman.sommer@fau.de>
2019-12-30 22:29:39 +0100

    iff: implement chunk parser, faulity lifetimes

--- src/iff.rs
+++ src/iff.rs
@@ -4,17 +4,29 @@ use std::io::Read;
 use std::path::Path;
 
 struct IFFChunk<'a> {
-	chunk_type: [char; 4],
-	length: u32, // TODO big endian conversion
+	chunk_type: [u8; 4],
 	data: & 'a mut [u8] // reference to other memory
 }
 
+impl<'a> IFFChunk<'a> {
+	fn parse(bytes: & 'a mut [u8]) -> (IFFChunk<'a>, usize) {
+		// initialize fixed size array from slice
+		let mut chunk_type : [u8; 4] = Default::default();
+		chunk_type.copy_from_slice(&bytes[..4]);
+		let mut data_size_bytes : [u8; 4] = Default::default();
+		data_size_bytes.copy_from_slice(&bytes[4..8]);
+		let data_size : usize = u32FromBEBytes(data_size_bytes) as usize;
+		let data = & mut bytes[8..8+data_size];
+		(IFFChunk { chunk_type: chunk_type, data: data }, 8 + data_size)
+	}
+}
+
 struct IFFFile<'a> {
-	chunks: Vec<& 'a mut IFFChunk<'a>>,
+	chunks: Vec<& 'a IFFChunk<'a>>,
 }
 
 impl<'a> IFFFile<'a> {
-	fn new(chunks: Vec<& 'a mut IFFChunk<'a>>) -> IFFFile<'a> {
+	fn new(chunks: Vec<& 'a IFFChunk<'a>>) -> IFFFile<'a> {
 		IFFFile {
 			chunks: chunks,
 		}
@@ -37,10 +49,24 @@ impl<'a> IFFFile<'a> {
 			Ok(n) => n,
 		};
 		// parse file
-		let mut chunks = Vec::<& mut IFFChunk>::new();
+		let mut chunks = Vec::<& IFFChunk>::new();
+		let mut offset : usize = 0;
 		loop {
-			
+			if offset + 8 > n_bytes {
+				break;
+			}
+			let (chunk, chunk_size) = IFFChunk::parse(& mut bytes[offset..]);
+			chunks.push(& chunk);
+			if chunk_size % 2 == 0 {
+				offset += chunk_size;
+			} else {
+				offset += chunk_size + 1;
+			}
 		}
-		let iff_file = IFFFile::new(Vec::new());
+		IFFFile::new(chunks)
 	}
+}
+
+fn u32FromBEBytes(bytes: [u8; 4]) -> u32 {
+	bytes[3] as u32 + (bytes[2] as u32) << 8 + (bytes[1] as u32) << 16 + (bytes[0] as u32) << 24
 }
