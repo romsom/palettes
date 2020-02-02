83d66a954407e7f6252574afdbf9fec3e48a7aa4
Roman Sommer <roman.sommer@fau.de>
2020-02-02 21:43:10 +0100

    implement recursive parsing of FORM chunks

--- src/iff.rs
+++ src/iff.rs
@@ -2,37 +2,144 @@ use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::fmt;
+//use std::default::Default;
 
-struct IFFChunk<'a> {
-	chunk_type: [u8; 4],
-	data: & 'a mut [u8] // reference to other memory
+#[derive(Debug)]
+pub struct IFFChunk {
+	pub chunk_type: String,
+	pub data: Vec<u8>, // TODO: reference to other memory
+	pub sub_chunks: Vec<IFFChunk>,
+	pub fourcc: Option<String>,
+	pub enumeration_complete: bool,
+	pub chunk_number: Option<usize>,
 }
 
-impl<'a> IFFChunk<'a> {
-	fn parse(bytes: & 'a mut [u8]) -> (IFFChunk<'a>, usize) {
-		// initialize fixed size array from slice
-		let mut chunk_type : [u8; 4] = Default::default();
-		chunk_type.copy_from_slice(&bytes[..4]);
-		let mut data_size_bytes : [u8; 4] = Default::default();
-		data_size_bytes.copy_from_slice(&bytes[4..8]);
-		let data_size : usize = u32FromBEBytes(data_size_bytes) as usize;
-		let data = & mut bytes[8..8+data_size];
-		(IFFChunk { chunk_type: chunk_type, data: data }, 8 + data_size)
+impl IFFChunk {
+	fn parse_4_bytes(ct: [u8; 4]) -> String {
+		String::from_utf8(ct.to_vec()).unwrap()
 	}
-}
 
-struct IFFFile<'a> {
-	chunks: Vec<& 'a IFFChunk<'a>>,
-}
-
-impl<'a> IFFFile<'a> {
-	fn new(chunks: Vec<& 'a IFFChunk<'a>>) -> IFFFile<'a> {
-		IFFFile {
-			chunks: chunks,
+	fn find_chunks(data: & Vec<u8>, chunks : & mut Vec<IFFChunk>) {
+		let mut offset : usize = 0;
+		let n_bytes : usize = data.len();
+		loop {
+			if offset + 8 > n_bytes {
+				break;
+			}
+			let (chunk, chunk_size) = IFFChunk::parse(& data[offset..]);
+			chunks.push(chunk);
+			if chunk_size % 2 == 0 {
+				offset += chunk_size;
+			} else {
+				offset += chunk_size + 1;
+			}
 		}
 	}
 
-	fn read_from_file(path: &Path) -> IFFFile<'a> {
+	fn parse(bytes: & [u8]) -> (IFFChunk, usize) {
+		// initialize fixed size array from slice
+
+		let header_size : usize;// = 8;
+		let mut chunk_tmp : [u8; 4] = [0, 0, 0, 0];
+		chunk_tmp.copy_from_slice(&bytes[..4]);
+		let chunk_type = IFFChunk::parse_4_bytes(chunk_tmp);
+
+		let mut data_size_bytes : [u8; 4] = Default::default();
+		data_size_bytes.copy_from_slice(&bytes[4..8]);
+		let mut data_size : usize = u32_from_be_bytes(data_size_bytes) as usize;
+
+		let fourcc =
+			if chunk_type == "FORM" && data_size >= 4 {
+				let mut fourcc_tmp : [u8; 4] = [0, 0, 0, 0];
+				fourcc_tmp.copy_from_slice(&bytes[8..12]);
+				Some(IFFChunk::parse_4_bytes(fourcc_tmp))
+			} else {
+				None
+			};
+
+		//print!("Found chunk with type: {}\n", chunk_type);
+		match & fourcc {
+			Some(fourcc_str) => {
+				header_size = 12;
+				data_size -= 4;
+				//print!("Chunk fourcc: {}\n", fourcc_str)
+			},
+			None => {
+				header_size = 8;
+				()
+			}
+		}
+		//print!("Chunk size {}\n", data_size);
+
+		//print!("Remaining bytes: {}\n", bytes.len());
+
+		let mut data : Vec<u8> = Vec::with_capacity(data_size);
+		// fill data with zeros
+		data.resize_with(data_size, Default::default);
+		if header_size + data_size <= bytes.len() {
+			data.copy_from_slice(&bytes[header_size..header_size+data_size]);
+		} else {
+			data.copy_from_slice(&bytes[header_size..]);
+			// print!("Truncated chunk: there were only {} bytes where there should have been {}\n",
+			//	   bytes.len() - header_size,
+			//	   data_size);
+		}
+		let mut sub_chunks : Vec<IFFChunk> = Vec::new();
+		if chunk_type == "FORM" {
+			IFFChunk::find_chunks(& data, & mut sub_chunks);
+		}
+		(IFFChunk { chunk_type: chunk_type, data: data, sub_chunks : sub_chunks, fourcc : fourcc, enumeration_complete: false, chunk_number: None }, 8 + data_size)
+	}
+
+	// enumerate chunks bredth-first
+	pub fn enumerate(cs: & mut Vec<IFFChunk>) {
+		let mut next : usize = 0;
+		let mut old : usize;
+		let mut level = 0;
+		loop {
+			old = next;
+			for c in & mut *cs {
+				next = IFFChunk::enumerate_rec(c, next, level);
+			}
+			level += 1;
+			if old == next {
+				break;
+			}
+		}
+	}
+
+	fn enumerate_rec(c : & mut IFFChunk, next_index : usize, level : usize) -> usize {
+		if c.enumeration_complete {
+			next_index
+		} else if level == 0 {
+			c.chunk_number = Some(next_index);
+			next_index + 1
+		} else {
+			let mut next = next_index;
+			for sc in & mut c.sub_chunks {
+				next = IFFChunk::enumerate_rec(sc, next, level - 1);
+			}
+			if next_index == next {
+				c.enumeration_complete = true;
+			}
+			next
+		}
+	}
+}
+
+impl std::fmt::Display for IFFChunk {
+	fn fmt(&self, f : & mut fmt::Formatter) -> fmt::Result {
+		write!(f, "Chunk {}, Type: {}, Size {}", self.chunk_number.unwrap(), self.chunk_type, self.data.len())
+	}
+}
+
+pub struct IFFFile {
+	pub chunks: Vec<IFFChunk>,
+}
+
+impl IFFFile {
+	pub fn read_from_file(path: &Path) -> IFFFile {
 		// open file
 		let mut file = match File::open(path) {
 			Err(why) => panic!("Could not open {}: {}",
@@ -42,31 +149,23 @@ impl<'a> IFFFile<'a> {
 		};
 		// read file contents
 		let mut bytes = Vec::new();
-		let n_bytes = match file.read_to_end(&mut bytes) {
+		// TODO: optimization: try to read 8 bytes for type and size, then size bytes, then repeat
+		let _n_bytes = match file.read_to_end(&mut bytes) {
 			Err(why) => panic!("Could not read {}: {}",
 							   path.display(),
 							   why.description()),
 			Ok(n) => n,
 		};
 		// parse file
-		let mut chunks = Vec::<& IFFChunk>::new();
-		let mut offset : usize = 0;
-		loop {
-			if offset + 8 > n_bytes {
-				break;
-			}
-			let (chunk, chunk_size) = IFFChunk::parse(& mut bytes[offset..]);
-			chunks.push(& chunk);
-			if chunk_size % 2 == 0 {
-				offset += chunk_size;
-			} else {
-				offset += chunk_size + 1;
-			}
+		let mut chunks = Vec::<IFFChunk>::new();
+		IFFChunk::find_chunks(& bytes, & mut chunks);
+		IFFChunk::enumerate(& mut chunks);
+		IFFFile {
+			chunks: chunks
 		}
-		IFFFile::new(chunks)
 	}
 }
 
-fn u32FromBEBytes(bytes: [u8; 4]) -> u32 {
-	bytes[3] as u32 + (bytes[2] as u32) << 8 + (bytes[1] as u32) << 16 + (bytes[0] as u32) << 24
+fn u32_from_be_bytes(bytes: [u8; 4]) -> u32 {
+	bytes[3] as u32 + ((bytes[2] as u32) << 8) + ((bytes[1] as u32) << 16) + ((bytes[0] as u32) << 24)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-extern crate sdl2; 
+extern crate sdl2;
 
 mod color_cycling;
 mod iff;
@@ -7,41 +7,56 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
- 
+
+use std::path::Path;
+
+pub fn print_chunks(cs : & Vec<iff::IFFChunk>) {
+	// recurse
+	for chunk in cs.iter() {
+		//print!("Chunk {}: Type: {}\n", chunk.chunk_number.unwrap(), chunk.chunk_type);
+		print!("{}\n", chunk);
+		print_chunks(& chunk.sub_chunks);
+	}
+}
+
 pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
- 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
- 
-    let mut canvas = window.into_canvas().build().unwrap();
- 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0u16;
-    'running: loop {
-        i = (i + 1) % 512;
+	let iff_file = iff::IFFFile::read_from_file(Path::new("./V08AM.LBM"));
+	print_chunks(& iff_file.chunks);
+
+
+	let sdl_context = sdl2::init().unwrap();
+	let video_subsystem = sdl_context.video().unwrap();
+
+	let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+		.position_centered()
+		.build()
+		.unwrap();
+
+	let mut canvas = window.into_canvas().build().unwrap();
+
+	canvas.set_draw_color(Color::RGB(0, 255, 255));
+	canvas.clear();
+	canvas.present();
+	let mut event_pump = sdl_context.event_pump().unwrap();
+	let mut i = 0u16;
+	'running: loop {
+		i = (i + 1) % 512;
 		let ind: u8 = if i < 256 { i } else { 511 - i} as u8;
 		let ind2: u8 = ((if ind < 128 { ind } else {255 - ind} as i8 * if i < 256 { -1 } else { 1 }) as i16 + 128i16) as u8;
-        canvas.set_draw_color(Color::RGB(ind, ind2 / 2, 255 - ind));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
-        // The rest of the game loop goes here...
+		canvas.set_draw_color(Color::RGB(ind, ind2 / 2, 255 - ind));
+		canvas.clear();
+		for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit {..} |
+				Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+					break 'running
+				},
+				_ => {}
+			}
+		}
+		// The rest of the game loop goes here...
 
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
+		canvas.present();
+		::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+	}
 }
